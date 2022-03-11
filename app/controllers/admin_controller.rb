06e3e70d68f9d629d2bd87f28bd7490c06bf63c1
mohamed amine mghirbi <mohamed.amine.mghirbi@gmail.com>
2022-03-11 11:00:33 +0100

    version 3

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -16,17 +16,13 @@ class AdminController < ApplicationController
     render json: @users 
   end
 
-
   def show
-  
-    @user = User.find(params[:id], current)
+    @user = User.find(params[:id])
     render json: @user
-  
+    
   end
 
   def update
-   
-    @user = User.find(params[:id])
     if @user.update(post_params)
       render json: @user
 
