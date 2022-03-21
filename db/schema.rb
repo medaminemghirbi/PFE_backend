3a900fc9708b04617ba31d2c57bdf4b4b9277f68
lakhal oumaima <lakhalouma6@gmail.com>
2022-03-21 12:20:54 +0100

    add-models-and-controllers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_21_102726) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_21_111936) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -92,7 +92,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_21_102726) do
 
   add_foreign_key "educations", "users"
   add_foreign_key "experiences", "users"
-  add_foreign_key "missions", "categories"
   add_foreign_key "missions", "users"
   add_foreign_key "reviews", "missions"
   add_foreign_key "reviews", "users"
