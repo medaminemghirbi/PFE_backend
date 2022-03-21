58f3a717c48538724d50c42380460897a71a4f0a
mohamed amine mghirbi <mohamed.amine.mghirbi@gmail.com>
2022-03-21 12:59:50 +0100

    FEATURE-lundi-modif

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,54 @@
+class AdminController < ApplicationController
+  include CurrentUserConcern
+
+  def index
+    if @current_user.role == "admin"
+      @users = User.all
+      render json: @users
+    else
+      render :json => 'you are not an admin'
+    end
+  end
+
+  def show
+    if @current_user.role == "admin"
+    @user = User.find(params[:id], current)
+    render json: @user
+  else
+    render :json => 'you are not an admin'
+  end
+  end
+
+  def update
+    if @current_user.role == "admin"
+    @user = User.find(params[:id])
+    if @user.update(post_params)
+      render json: @user
+
+    else
+      render json: @user.errors, statut: :unprocessable_entity
+    end
+  else
+    render :json => 'you are not an admin'
+  end
+  end
+
+  def destroy
+    if @current_user.role == "admin"
+    @user = User.find(params[:id])
+    @user.destroy
+  else
+    render :json => 'you are not an admin'
+  end
+  end
+
+  private
+
+  def post_params
+    params.require(:user).permit!
+  end
+
+  def set_post
+    @user = User.find(params[:id])
+  end
+end
---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,25 @@
+class RegistrationsController < ApplicationController
+  def create
+    user = User.create!(
+      firstname: params['firstname'],
+      lastname: params['lastname'],
+      email: params['email'],
+      password: params['password'],
+      adresse: params['adresse'],
+      password_confirmation: params['password_confirmation'],
+      role: params['role'].to_i
+    )
+
+    if user
+      session[:user_id] = user.id
+
+      render json: {
+        status: :created,
+
+        user: user
+      }
+    else
+      render json: { status: 500 }
+    end
+  end
+end
