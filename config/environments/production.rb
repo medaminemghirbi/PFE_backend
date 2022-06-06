78f6171a976531771a2328afd6e1b8270d19176c
mohamed amine mghirbi <mohamed.amine.mghirbi@gmail.com>
2022-06-06 18:47:56 +0100

    add prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,23 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_files = false
+
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
+
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = true
+
+  config.assets.initialize_on_precompile = true
+
+  # Generate digests for assets URLs
+  config.assets.digest = true
+
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
