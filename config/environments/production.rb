ce3add5fcac8e01d888f6fd6d7224cef3bca3f14
mohamed amine mghirbi <mohamed.amine.mghirbi@gmail.com>
2022-06-07 15:56:08 +0100

    edit production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,22 +6,6 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_files = false
-
-  # Compress JavaScripts and CSS
-  config.assets.compress = true
-
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
-
-  config.assets.initialize_on_precompile = true
-
-  # Generate digests for assets URLs
-  config.assets.digest = true
-
 
   #https://devcenter.heroku.com/articles/getting-started-with-rails6 ( Heroku gems )
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
