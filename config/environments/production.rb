ae70fe565688565a094dcef2863916bc3a611db2
mohamed amine mghirbi <mohamed.amine.mghirbi@gmail.com>
2022-06-07 16:11:24 +0100

    fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-  host = 'example.com' #replace with your own url
+  host = 'https://freelancyback.herokuapp.com/' #replace with your own url
   config.action_mailer.default_url_options = { host: host }
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
